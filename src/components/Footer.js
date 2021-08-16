310e684bebc98364c55a10e5252fa5991d0440be
monship10 <monchijgarcia@gmail.com>
2021-08-16 12:23:48 -0400

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,10 @@ const Footer = () => {
     return (
         <footer className ="footer">
            
-               <a href ="/" target="_blank">Github</a>
-               <a href="/" target="_blank">Instagram</a>
-               <a href="/" target="_blank">Linkedin</a>
-               <a href="/" target="_blank">Twitch</a>
-               <a href="/" target="_blank">Twitter</a>
+               <a href ="https://github.com/RamonDuran10" rel="noreferrer"  target="_blank">Github</a>
+               <a href="https://www.instagram.com/monship/?hl=es-la" rel="noreferrer"  target="_blank">Instagram</a>
+               <a href="https://www.linkedin.com/in/ramon-jose-duran-garcia-298657215/" rel="noreferrer"  target="_blank">Linkedin</a>
+               <a href="https://m.twitch.tv/monship/home" rel="noreferrer"  target="_blank">Twitch</a>
                <p>copyright by Ramon Duran 2021</p>
            
         </footer>
